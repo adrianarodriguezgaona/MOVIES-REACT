dd105beef3fe1f056abc1ebafb987f28a5d80860
Adriana <Adrianitargaona@gmail.com>
2021-12-14 12:33:51 +0100

    component and modules add to show an image movie

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,3 +1,11 @@
-export function MovieCard({movie}){
-    return <li>{movie.title}</li>
+import styles from './MovieCard.module.css';
+
+export function MovieCard({ movie }) {
+  const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
+  return (
+    <li className= {styles.movieCard}>
+      <img  width={300} height={450}  className={styles.movieImage} src={imageUrl} alt={movie.title} />
+      <div>{movie.title}</div>
+    </li>
+  );
 }
--- src/MoviesGrid.jsx
+++ src/MoviesGrid.jsx
@@ -1,9 +1,10 @@
 import movies from './movies.json';
 import { MovieCard } from './MovieCard';
+import styles from './MoviesGrid.module.css';
 
 export function MoviesGrid() {
   return (
-    <ul>
+    <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
       ))}
