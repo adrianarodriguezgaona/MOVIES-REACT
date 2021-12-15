13deab83a10fc6ab3c395b2e950e5fd694132c9f
Adriana <Adrianitargaona@gmail.com>
2021-12-15 13:55:14 +0100

    Introducing routing with react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { MoviesGrid } from "./MoviesGrid";
+import { MoviesGrid } from "./components/MoviesGrid";
 import styles from './App.module.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
+
 
 export function App(){
-    return <div>
+    return <Router>
         <header>
+            <Link to="/"> 
             <h1 className={styles.title}>Movies</h1>
+            </Link>
         </header>
         <main>
-           <MoviesGrid/>
+        <Switch>
+          <Route path="/movie/:movieId"><MovieDetails/></Route>
+          <Route path="/"><LandingPage/></Route>
+        </Switch>
         </main>
-    </div>
+    </Router>
 }
